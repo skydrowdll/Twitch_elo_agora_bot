30e2b70316474e87e6bfbfabfee4ef3194518652
SkyDrow <33040484+skydrowdll@users.noreply.github.com>
2018-01-12 18:25:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ var options = {
 		username: "EloStore",
 		password: process.env.BOT_TOKEN // https://twitchapps.com - pour creer sa cle oauth twitch
 	},
-	channels: ['#skydrowdll']
+	channels: [process.env.NAME_CHANNEL]
 }
 var client = new tmi.client(options);
 client.connect();
@@ -37,7 +37,7 @@ client.connect();
 client.on("chat", (channel, user, message, self) =>{
 	if(self) return
 
-	if(channel = "#skydrowdll"){
+	if(channel = process.env.NAME_CHANNEL){ // #skydrowdll - nom du channel
 		var n = message.includes(prefix);
 		if(n){
 			mystring = message.replace(prefix+" ",'');
@@ -72,22 +72,22 @@ client.on("chat", (channel, user, message, self) =>{
 						        if(elo>=1700){rank = "diams"}
 						        if(elo>=2200){rank = "master"}
 						        
-						        if(rank=="bronze"){client.say("#skydrowdll", "Tu es Bronze avec "+elo+" elo.")}
-						        if(rank=="silver"){client.say("#skydrowdll", "Tu es Silver avec "+elo+" elo.")}
-						        if(rank=="gold"){client.say("#skydrowdll", "Tu es Gold avec "+elo+" elo.")}
-						        if(rank=="platin"){client.say("#skydrowdll", "Tu es Platine avec "+elo+" elo.")}
-						        if(rank=="diams"){client.say("#skydrowdll", "Tu es Diams avec "+elo+" elo.")}
-						        if(rank=="master"){client.say("#skydrowdll", "Tu es Master avec "+elo+" elo. (tu déchire tous là! fais une pause non ? :p")}
+						        if(rank=="bronze"){client.say(process.env.NAME_CHANNEL", "Tu es Bronze avec "+elo+" elo.")}
+						        if(rank=="silver"){client.say(process.env.NAME_CHANNEL, "Tu es Silver avec "+elo+" elo.")}
+						        if(rank=="gold"){client.say(process.env.NAME_CHANNEL, "Tu es Gold avec "+elo+" elo.")}
+						        if(rank=="platin"){client.say(process.env.NAME_CHANNEL, "Tu es Platine avec "+elo+" elo.")}
+						        if(rank=="diams"){client.say(process.env.NAME_CHANNEL, "Tu es Diams avec "+elo+" elo.")}
+						        if(rank=="master"){client.say(process.env.NAME_CHANNEL, "Tu es Master avec "+elo+" elo. (tu déchire tous là! fais une pause non ? :p")}
 						        console.log("========")
 						        console.log(elo)
 						        console.log("========")
 						    }else{
-						    	client.say("#skydrowdll", "Error")
+						    	client.say(process.env.NAME_CHANNEL, "Error")
 						    }
 						})
 
 			       }catch(err){
-			 			client.say("#skydrowdll", "Le compte est privé ou le joueur en question n'existe pas!")
+			 			client.say(process.env.NAME_CHANNEL, "Le compte est privé ou le joueur en question n'existe pas!")
 			       }
 			    }
 			})
